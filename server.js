c8460ca56e36430bde7bbf82ed4c48b632b68ffd
unknown <rafael.giannico@gmail.com>
2023-09-25 21:51:49 -0300

    Alteração de aplicação local para aplicação na nuvem

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3002;
 
 // Dados das cidades
 const cityData = require('./cityData.json');
 
-app.use(express.static('public'));
+// Serve os arquivos estáticos do frontend
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Rota para obter dados da cidade
-
 app.get('/city/:name', (req, res) => {
-    const cityName = req.params.name;
-    const data = cityData[cityName];
-    if (data) {
-      res.json(data);
-    } else {
-      res.status(404).send('City not found');
-    }
-  });
+  const cityName = req.params.name;
+  const data = cityData[cityName];
+  if (data) {
+    res.json(data);
+  } else {
+    res.status(404).send('City not found');
+  }
+});
+
+// Captura todas as outras rotas e retorna o index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
